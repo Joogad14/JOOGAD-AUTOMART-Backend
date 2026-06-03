e45a8442baf3f2b1032a44b2fc297eacfee54034
Joogad14 <olutobideyemi14@gmail.com>
2026-06-03 05:28:40 -0700

    fix email config

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,35 +2,31 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-
-  port: process.env.EMAIL_PORT,
-
+  port: Number(process.env.EMAIL_PORT),
   secure: false,
+  requireTLS: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendEmail = async (options) => {
-
   try {
-
     await transporter.sendMail({
       from: `"JOOGAD AUTOMART" <${process.env.EMAIL_USER}>`,
-
       to: options.to,
-
       subject: options.subject,
-
       text: options.text,
     });
 
     console.log("EMAIL SENT SUCCESSFULLY");
-
   } catch (error) {
-
     console.log("EMAIL ERROR:", error.message);
   }
 };
